ede6490f2d08450cee255a7bf3978a8b6b7040b8
mohammad salehi <saeed.salehi86@gmail.com>
2019-10-31 14:20:27 +0330

    add modele Income and add Sql

--- web/models.py
+++ web/models.py
@@ -7,3 +7,9 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
